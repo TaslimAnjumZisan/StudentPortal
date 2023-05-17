3e159989325a991ab43a9c73b8a4b224eef78a77
TasnimAnjum <114102739+TasnimAnjum@users.noreply.github.com>
2023-05-17 17:46:35 +0600

    stak in creating object

---
+++ Asset/js/index.js
@@ -0,0 +1,9 @@
+function register() {
+    var name1 = document.getElementById("name").value;
+    var rol = document.getElementById("roll").value;
+    var dep = document.getElementById("dept").value;
+    var phn = document.getElementById("phn").value;
+    var eml = document.getElementById("email1").value;
+    var pas = document.getElementById("Password").value;
+    
+}
