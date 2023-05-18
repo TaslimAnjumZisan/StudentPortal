0d6989f6449aa08777ed0f0b17d46cc6ec745e8e
TasnimAnjum <114102739+TasnimAnjum@users.noreply.github.com>
2023-05-18 17:27:43 +0600

    in one page website

--- Asset/js/index.js
+++ Asset/js/index.js
@@ -1,9 +1,74 @@
+var studentList = [];
+// localStorage.setItem('students', "");
 function register() {
-    var name1 = document.getElementById("name").value;
-    var rol = document.getElementById("roll").value;
-    var dep = document.getElementById("dept").value;
-    var phn = document.getElementById("phn").value;
-    var eml = document.getElementById("email1").value;
-    var pas = document.getElementById("Password").value;
-    
+    var newObject = new Object();
+    newObject.name=document.getElementById("name").value;
+    newObject.roll=document.getElementById("roll").value;
+    newObject.department=document.getElementById("dept").value;
+    newObject.phone=document.getElementById("phn").value;
+    newObject.email=document.getElementById("email1").value;
+    newObject.password=document.getElementById("Password").value;
+
+studentList.push(newObject);
+console.log(studentList);
+
 }
+
+function goBack(){
+    console.log(studentList);
+}
+
+function saveObjects() {
+    localStorage.setItem('students', JSON.stringify(studentList));
+}
+  
+function login(){
+    var checkEmail = document.getElementById("email").value;
+    var checkPas = document.getElementById("Password1").value;
+    var student={
+        
+    }
+    alert(checkEmail);
+    login1(checkEmail,checkPas);
+}
+
+function login1(email, password) {
+    
+    // alert(email);
+    // alert(password);
+
+    // let datas=localStorage.students;
+    // alert(datas);
+    // datas.forEach(data => {
+    //     alert(data.email);
+    //     if(data.email===checkEmail)
+    //     {
+    //         alert(data.email);
+    //     }
+    // });
+    let loggedInUser = students.find(data => data.email === email);
+    alert("loggedInUser");
+  
+    if (loggedInUser && loggedInUser.password === password) {
+      
+      alert("login successfull")
+    } else {
+      
+      alert("email or pass not matched")
+    }
+}
+
+$(document).ready(function(){
+    $('.btns #btn1').click(function () {
+        $('.reg').show();
+        $('.nav').hide();
+    });
+    $('.btns #btn2').click(function () {
+        $('.login').show();
+        $('.nav').hide();
+    });
+
+    // $('button').click(function(){
+    //     $('.nav').show();
+    // });
+});
